8481f37e9106ef99f3bbc11e1528731131b0a55b
peepo325 <68525284+peepo325@users.noreply.github.com>
2020-08-18 13:03:11 +0700

    Game

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <div>
+        <Random/>
+     </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Random from './components/Random.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Random
   }
 }
 </script>
@@ -22,7 +24,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ff0000;
   margin-top: 60px;
 }
+
 </style>
